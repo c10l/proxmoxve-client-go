e889d6b9e8b3074852ce2c0a6ed6baf7c057d458
c10l <git@c10l.cc>
2022-06-03 21:52:01 +0100

    Re-consolidate files per api endpoint

--- api2/pools_test.go
+++ api2/pools_test.go
@@ -197,3 +197,46 @@ func TestPoolListRetrieve(t *testing.T) {
 	assert.Contains(t, poolList, Pool{PoolID: poolID1, Comment: ""})
 	assert.Contains(t, poolList, Pool{PoolID: poolID2, Comment: ""})
 }
+
+func TestPoolGet(t *testing.T) {
+	poolID := rand.String(10)
+	expectedComment := rand.String(20)
+	err := testClient.PostPool(poolID, expectedComment)
+	assert.NoError(t, err)
+
+	pool, err := testClient.GetPool(poolID)
+	assert.NoError(t, err)
+	assert.Equal(t, poolID, pool.PoolID)
+	assert.Equal(t, expectedComment, pool.Comment)
+}
+
+func TestPoolPut(t *testing.T) {
+	poolID := rand.String(10)
+	expectedComment := rand.String(20)
+
+	err := testClient.PostPool(poolID, expectedComment)
+	assert.NoError(t, err)
+
+	err = testClient.PutPool(poolID, &expectedComment, nil, nil, false)
+	assert.NoError(t, err)
+
+	pool, err := testClient.GetPool(poolID)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedComment, pool.Comment)
+}
+
+func TestPoolDelete(t *testing.T) {
+	poolID := rand.String(10)
+
+	err := testClient.PostPool(poolID, "")
+	assert.NoError(t, err)
+
+	_, err = testClient.GetPool(poolID)
+	assert.NoError(t, err)
+
+	err = testClient.DeletePool(poolID)
+	assert.NoError(t, err)
+
+	_, err = testClient.GetPool(poolID)
+	assert.ErrorContains(t, err, fmt.Sprintf("500 pool '%s' does not exist", poolID))
+}
