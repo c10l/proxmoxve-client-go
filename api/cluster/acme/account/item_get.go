a27ea08e9a528b2bef266aa62188a3399ebdea7e
c10l <git@c10l.cc>
2022-07-05 15:59:02 +0100

    Add ACME account get

---
+++ api/cluster/acme/account/item_get.go
@@ -0,0 +1,56 @@
+package account
+
+import (
+	"encoding/json"
+
+	"github.com/c10l/proxmoxve-client-go/api"
+)
+
+type ItemGetRequest struct {
+	Client *api.Client
+
+	Name *string
+}
+
+type ItemGetResponse struct {
+	Account   ItemGetResponseAccount `json:"account"`
+	Directory string                 `json:"directory"`
+	Location  string                 `json:"location"`
+	TOS       string                 `json:"tos"`
+}
+
+type ItemGetResponseAccount struct {
+	Contact   []string                  `json:"contact"`
+	InitialIP string                    `json:"initialIp"`
+	Status    string                    `json:"status"`
+	CreatedAt string                    `json:"createdAt"`
+	Key       ItemGetResponseAccountKey `json:"key"`
+}
+
+type ItemGetResponseAccountKey struct {
+	E   string `json:"e"`
+	N   string `json:"n"`
+	Use string `json:"use"`
+	Kty string `json:"kty"`
+}
+
+// GetItem satisfies the ItemGetter interface
+// Not to be used directly. Use Get() instead.
+func (g ItemGetRequest) GetItem() ([]byte, error) {
+	var name string
+	if g.Name == nil {
+		name = "default"
+	} else {
+		name = *g.Name
+	}
+	return g.Client.GetItem(g, basePath, name)
+}
+
+func (g ItemGetRequest) Get() (*ItemGetResponse, error) {
+	item, err := g.GetItem()
+	if err != nil {
+		return nil, err
+	}
+	resp := new(ItemGetResponse)
+	return resp, json.Unmarshal(item, resp)
+}
---
+++ api/cluster/acme/account/item_get_test.go
@@ -0,0 +1,33 @@
+package account
+
+import (
+	"testing"
+	"time"
+
+	"github.com/c10l/proxmoxve-client-go/helpers"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/util/rand"
+)
+
+func TestItemGet(t *testing.T) {
+	req := PostRequest{
+		Client:    helpers.TicketTestClient(),
+		Name:      helpers.PtrTo("pmvetest_acme_" + rand.String(10)),
+		Contact:   "foobar@baz.com",
+		Directory: helpers.PtrTo("https://127.0.0.1:14000/dir"),
+		TOSurl:    helpers.PtrTo("https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf"),
+	}
+	_, err := req.Post()
+	assert.NoError(t, err)
+
+	var account *ItemGetResponse
+	assert.Eventually(t, func() bool {
+		var err error
+		account, err = ItemGetRequest{Client: helpers.TicketTestClient(), Name: req.Name}.Get()
+		return assert.NoError(t, err) &&
+			assert.Equal(t, *req.Directory, account.Directory) &&
+			len(account.Account.Contact) > 0 &&
+			assert.Equal(t, "mailto:"+req.Contact, account.Account.Contact[0])
+	}, 10*time.Second, 500*time.Millisecond, err)
+
+}
