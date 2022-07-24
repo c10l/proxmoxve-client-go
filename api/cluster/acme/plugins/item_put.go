c829edf8964b24fd0f9bc43a90f6039818e998a1
c10l <git@c10l.cc>
2022-07-24 13:47:09 -0300

    Implement acme plugins item put

---
+++ api/cluster/acme/plugins/item_put.go
@@ -0,0 +1,67 @@
+package plugins
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/c10l/proxmoxve-client-go/api"
+	"github.com/c10l/proxmoxve-client-go/helpers"
+)
+
+type ItemPutRequest struct {
+	Client *api.Client
+	ID     string
+
+	// Optional arguments
+	API             *string
+	Data            *string
+	Delete          *string
+	Digest          *string
+	Disable         *helpers.IntBool
+	Nodes           *[]string
+	ValidationDelay *int
+}
+
+// PutItem satisfies the ItemPutter interface.
+// Not to be used directly. Use Put() instead.
+func (g ItemPutRequest) PutItem() ([]byte, error) {
+	return g.Client.PutItem(g, basePath, g.ID)
+}
+
+func (g ItemPutRequest) Put() error {
+	_, err := g.PutItem()
+	return err
+}
+
+// ParseParams satisfies the ItemPutter interface.
+// Not to be used directly. Use Put() instead.
+func (g ItemPutRequest) ParseParams(apiURL *url.URL) error {
+	params := url.Values{}
+	if g.API != nil {
+		params.Add("api", *g.API)
+	}
+	if g.Data != nil {
+		params.Add("data", *g.Data)
+	}
+	if g.Delete != nil {
+		params.Add("delete", *g.Delete)
+	}
+	if g.Digest != nil {
+		params.Add("digest", *g.Digest)
+	}
+	if g.Disable != nil {
+		params.Add("disable", g.Disable.IntAsString())
+	}
+	if g.Nodes != nil {
+		params.Add("nodes", strings.Join(*g.Nodes, ","))
+	}
+	if g.ValidationDelay != nil {
+		params.Add("validation-delay", fmt.Sprintf("%d", *g.ValidationDelay))
+	}
+	if len(params) == 0 {
+		return fmt.Errorf("no params")
+	}
+	apiURL.RawQuery = params.Encode()
+	return nil
+}
---
+++ api/cluster/acme/plugins/item_put_test.go
@@ -0,0 +1,38 @@
+package plugins
+
+import (
+	"testing"
+	"time"
+
+	"github.com/c10l/proxmoxve-client-go/helpers"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/util/rand"
+)
+
+func TestItemPut(t *testing.T) {
+	postReq := PostRequest{
+		Client: helpers.TicketTestClient(),
+		ID:     "pmvetest_acme_" + rand.String(10),
+		Type:   "standalone",
+	}
+	err := postReq.Post()
+	assert.NoError(t, err)
+
+	assert.Eventually(t, func() bool {
+		_, err := ItemGetRequest{Client: helpers.APITokenTestClient(), ID: postReq.ID}.Get()
+		return err == nil
+	}, 5*time.Second, 500*time.Millisecond, err)
+
+	itemPutReq := ItemPutRequest{
+		Client:  helpers.TicketTestClient(),
+		ID:      postReq.ID,
+		Nodes:   &[]string{"node1", "node2"},
+		Disable: helpers.PtrTo(helpers.IntBool(true)),
+	}
+	err = itemPutReq.Put()
+	assert.NoError(t, err)
+	item, err := ItemGetRequest{Client: helpers.APITokenTestClient(), ID: postReq.ID}.Get()
+	assert.NoError(t, err)
+	assert.Equal(t, true, item.Disable.Bool())
+	assert.Equal(t, "node1,node2", item.Nodes)
+}
