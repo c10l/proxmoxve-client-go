0876349ae452ca41f0e4c54714bb76ecd687dc73
c10l <git@c10l.cc>
2022-07-23 21:19:00 -0300

    Fix test

--- api/cluster/acme/plugins/get_test.go
+++ api/cluster/acme/plugins/get_test.go
@@ -11,5 +11,5 @@ func TestGet(t *testing.T) {
 	accountList, err := GetRequest{Client: helpers.APITokenTestClient()}.Get()
 	assert.NoError(t, err)
 	assert.NotNil(t, accountList)
-	assert.Equal(t, "standalone", accountList[0].Plugin)
+	assert.Equal(t, "standalone", accountList[0].Type)
 }
